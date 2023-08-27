a31374ab4a6fdee06ebd276c91d0664b9eaf6bb4
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-27 20:51:40 +0300

    refactor: configs

--- src/main/java/com/gucardev/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/gucardev/springbootdemo/SpringBootDemoApplication.java
@@ -2,12 +2,13 @@ package com.gucardev.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication  extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
 
 }
